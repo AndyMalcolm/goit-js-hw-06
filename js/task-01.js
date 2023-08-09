8b6eebb0d03092f052545150fd7da0816f63db6e
AndyMalcolm <ayhrunin.fitu21@kubg.edu.ua>
2023-08-09 21:13:30 +0300

    hm

--- js/task-01.js
+++ js/task-01.js
@@ -3,6 +3,7 @@ const categories = ['Animals', 'Products', 'Technologies'];
 const Animals = ['Cat', 'Hamster', 'Horse', 'Parrot'];
 const Products = ['Bread', 'Prasley', 'Cheese'];
 const Technologies = ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js'];
+const category = document.querySelector("#categories");
 Animals.forEach(function() {
   console.log(`Number of categories: ${categories.length}
 Category: ${categories[0]}
