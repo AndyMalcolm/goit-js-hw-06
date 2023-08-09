d765cc664bc30b5046c264ade6f6d02563be27e7
AndyMalcolm <ayhrunin.fitu21@kubg.edu.ua>
2023-08-09 10:29:36 +0300

    так

--- js/task-01.js
+++ js/task-01.js
@@ -1,25 +1,11 @@
-Напиши скрипт, який:
 
-Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
-Для кожного элемента li.item у списку ul#categories, знайде і виведе в консоль текст заголовку елемента (тегу <h2>) і кількість елементів в категорії (усіх <li>, вкладених в нього).
-Для виконання цього завдання потрібно використати метод forEach() і властивості навігації по DOM.
-
-В результаті, в консолі будуть виведені наступні повідомлення.
-
-Number of categories: 3
-
-Category: Animals
-Elements: 4
-
-Category: Products
-Elements: 3
-
-Category: Technologies
-Elements: 5
-
-const array = Animals;
-
-array.forEach(function(element, index) {
-  console.log(`Category: ${h2}
-  Elements: ${li}`);
+const categories = ['Animals', 'Products', 'Technologies'];
+const Animals = ['Cat', 'Hamster', 'Horse', 'Parrot'];
+const Products = ['Bread', 'Prasley', 'Cheese'];
+const Technologies = ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js'];
+Animals.forEach(function() {
+  console.log(`Number of categories: ${categories.length}
+  \nCategory: Animals\nElements: ${Animals.length}
+  \nCategory: Products\nElements: ${Products.length}
+  \nCategory: Technologies\nElements: ${Technologies.length}`);
 });
