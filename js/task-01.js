78d31c4c9d22517fa5f628b65b2794e852d756a5
AndyMalcolm <ayhrunin.fitu21@kubg.edu.ua>
2023-08-09 11:04:52 +0300

    opa

--- js/task-01.js
+++ js/task-01.js
@@ -5,7 +5,10 @@ const Products = ['Bread', 'Prasley', 'Cheese'];
 const Technologies = ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js'];
 Animals.forEach(function() {
   console.log(`Number of categories: ${categories.length}
-  \nCategory: Animals\nElements: ${Animals.length}
-  \nCategory: Products\nElements: ${Products.length}
-  \nCategory: Technologies\nElements: ${Technologies.length}`);
+Category: Animals
+Elements: ${Animals.length}
+Category: Products
+Elements: ${Products.length}
+Category: Technologies
+Elements: ${Technologies.length}`);
 });
