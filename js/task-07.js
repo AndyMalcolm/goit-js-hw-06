d0f6843d68d44c09afc7bbfff42f8fc670fe7baa
AndyMalcolm <ayhrunin.fitu21@kubg.edu.ua>
2023-08-13 17:44:56 +0300

    done

---
+++ js/task-07.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const fontSizeControl = document.getElementById('font-size-control');
+    const textElement = document.getElementById('text');
+  
+    fontSizeControl.addEventListener('input', function () {
+      const fontSizeValue = this.value + 'px';
+      textElement.style.fontSize = fontSizeValue;
+    });
+  });
+  
