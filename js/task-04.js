ccb32a3a56bad12e4f8a9e09d13a25d9808897e7
AndyMalcolm <ayhrunin.fitu21@kubg.edu.ua>
2023-08-13 12:14:44 +0300

    упд

--- js/task-04.js
+++ js/task-04.js
@@ -1,5 +1,16 @@
 /**Створи змінну counterValue, в якій буде зберігатися поточне значення лічильника та ініціалізуй її значенням 0.
 Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника.
 Оновлюй інтерфейс новим значенням змінної counterValue. */
-const counterValue = 0;
-const valueSpan = document.getElementById("value");
+const decrementButton = document.querySelector('[data-action="decrement"]');
+const incrementButton = document.querySelector('[data-action="increment"]');
+const valueSpan = document.getElementById('value');
+
+let counterValue = 0;
+
+decrementButton.addEventListener('click', () => {
+  valueSpan.textContent = --counterValue;
+});
+
+incrementButton.addEventListener('click', () => {
+  valueSpan.textContent = ++counterValue;
+});
