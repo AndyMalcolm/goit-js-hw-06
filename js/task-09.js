293d5609c067a4530cc221fcd87fa33de68b1aa8
AndyMalcolm <ayhrunin.fitu21@kubg.edu.ua>
2023-08-13 18:14:13 +0300

    tak?

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,27 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+document.addEventListener('DOMContentLoaded', function () {
+  const loginForm = document.querySelector('.login-form');
+
+  loginForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const emailInput = this.elements.email;
+    const passwordInput = this.elements.password;
+
+    if (emailInput.value === '' || passwordInput.value === '') {
+      alert('All fields must be filled out.');
+      return;
+    }
+
+    const loginData = {
+      email: emailInput.value,
+      password: passwordInput.value,
+      color: getRandomHexColor(),
+    };
+
+    console.log(loginData);
+    this.reset();
+  });
+});
