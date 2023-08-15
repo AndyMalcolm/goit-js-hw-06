3677a33c6fa0647ed7f99dd965f81baf14f72d28
AndyMalcolm <ayhrunin.fitu21@kubg.edu.ua>
2023-08-15 23:45:28 +0300

    eh

--- js/task-01.js
+++ js/task-01.js
@@ -1,16 +1,16 @@
 
-// const categories = ['Animals', 'Products', 'Technologies'];
-// const Animals = ['Cat', 'Hamster', 'Horse', 'Parrot'];
-// const Products = ['Bread', 'Prasley', 'Cheese'];
-// const Technologies = ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js'];
-// const category = document.querySelector("#categories");
-// Animals.forEach(function() {
-//   console.log(`Number of categories: ${categories.length}
-// Category: ${categories[0]}
-// Elements: ${Animals.length}
-// Category: ${categories[1]}
-// Elements: ${Products.length}
-// Category: ${categories[2]}
-// Elements: ${Technologies.length}`);
-// });
+const categories = ['Animals', 'Products', 'Technologies'];
+const Animals = ['Cat', 'Hamster', 'Horse', 'Parrot'];
+const Products = ['Bread', 'Prasley', 'Cheese'];
+const Technologies = ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js'];
+const category = document.querySelector("#categories");
+Animals.forEach(function() {
+  console.log(`Number of categories: ${categories.length}
+Category: ${categories[0]}
+Elements: ${Animals.length}
+Category: ${categories[1]}
+Elements: ${Products.length}
+Category: ${categories[2]}
+Elements: ${Technologies.length}`);
+});
 //переделать
