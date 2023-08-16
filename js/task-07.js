25c482ffcfd38eca9d93a32b81c869209733f452
AndyMalcolm <ayhrunin.fitu21@kubg.edu.ua>
2023-08-16 20:55:21 +0300

    эх

--- js/task-07.js
+++ js/task-07.js
@@ -1,10 +1,10 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const fontSizeControl = document.getElementById('font-size-control');
-    const textElement = document.getElementById('text');
+// document.addEventListener('DOMContentLoaded', function () {
+//     const fontSizeControl = document.getElementById('font-size-control');
+//     const textElement = document.getElementById('text');
   
-    fontSizeControl.addEventListener('input', function () {
-      const fontSizeValue = this.value + 'px';
-      textElement.style.fontSize = fontSizeValue;
-    });
-  });
+//     fontSizeControl.addEventListener('input', function () {
+//       const fontSizeValue = this.value + 'px';
+//       textElement.style.fontSize = fontSizeValue;
+//     });
+//   });
 // функцию сделать именнованой и вынести из скобок
