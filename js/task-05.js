15c1389c5f4d50427fc8fa1add8ad610fa5a5ac7
AndyMalcolm <ayhrunin.fitu21@kubg.edu.ua>
2023-08-17 23:53:04 +0300

    найс

--- js/task-05.js
+++ js/task-05.js
@@ -1,19 +1,7 @@
-// const nameInput = document.getElementById("name-input");
-// const nameOutput = document.getElementById("name-output");
-
-// nameInput.addEventListener("input", () => {
-//   if (nameInput.value === "") {
-//     nameOutput.textContent = "Anonymous";
-//   } else {
-//     nameOutput.textContent = nameInput.value.trim();
-//   }
-// });
 const nameInput = document.getElementById("name-input");
 const nameOutput = document.getElementById("name-output");
-
 nameInput.addEventListener("input", () => {
   const trimmedValue = nameInput.value.trim();
-  
   if (trimmedValue === "") {
     nameOutput.textContent = "Anonymous";
   } else {
