5634f9d8e715fcc5bf3f9ef810cb6ad2ab5f05da
AndyMalcolm <ayhrunin.fitu21@kubg.edu.ua>
2023-08-17 22:23:09 +0300

    ух

--- js/task-01.js
+++ js/task-01.js
@@ -1,19 +1,12 @@
-
-// const categories = ['Animals', 'Products', 'Technologies'];
-// const Animals = ['Cat', 'Hamster', 'Horse', 'Parrot'];
-// const Products = ['Bread', 'Prasley', 'Cheese'];
-// const Technologies = ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js'];
-// const category = document.querySelector("#categories");
-// Animals.forEach(function() {
-//   console.log(`Number of categories: ${categories.length}
-// Category: ${categories[0]}
-// Elements: ${Animals.length}
-// Category: ${categories[1]}
-// Elements: ${Products.length}
-// Category: ${categories[2]}
-// Elements: ${Technologies.length}`);
+// const categoriesList = document.querySelectorAll('.item');
+// console.log(`Number of categories: ${categoriesList.length}`);
+// categoriesList.forEach(category => {
+//   const categoryName = category.querySelector('h2').textContent;
+//   const categoryItems = category.querySelectorAll('li').length;
+//   console.log(`Category: ${categoryName}`);
+//   console.log(`Elements: ${categoryItems}`);
 // });
-const categoriesList = document.querySelectorAll('.item'); // Отримуємо всі елементи li.item
+const categoriesList = document.querySelectorAll('.item');
 console.log(`Number of categories: ${categoriesList.length}`);
 categoriesList.forEach(category => {
   const categoryName = category.querySelector('h2').textContent;
