a15046394bbd47887fe3d02b477d7e5e6640d23d
AndyMalcolm <ayhrunin.fitu21@kubg.edu.ua>
2023-08-20 00:14:19 +0300

    да

--- js/task-01.js
+++ js/task-01.js
@@ -10,10 +10,8 @@ const categoriesContainer = document.querySelector('.categories-container');
 const categoriesList = categoriesContainer.querySelectorAll('.item');
 console.log(`Number of categories: ${categoriesList.length}`);
 categoriesList.forEach(category => {
-  // const categoryName = category.querySelector('h2').textContent;
-  // const categoryItems = category.querySelectorAll('li').length;
-  const categoryName = category.firstElementChild('h2').textContent;
-  const categoryItems = category.lastElementChild('li').length;
+  const categoryName = category.firstElementChild.textContent;
+  const categoryItems = category.lastElementChild.length;//вместо querySelectorAll('li')
   console.log(`Category: ${categoryName}`);
   console.log(`Elements: ${categoryItems}`);
 });
